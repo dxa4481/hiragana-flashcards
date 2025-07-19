cdd4da084575bde5657c7f3c2a4800053488c401
couch <couch@ccps-MacBook-Air.local>
2025-07-19 06:28:45 -0700

    cleaning up

--- vocab-words/FlashcardCard.jsx
+++ vocab-words/FlashcardCard.jsx
@@ -112,7 +112,7 @@
           </button>
           <button
             onClick={playAudio}
-            className="bg-emerald-500 text-white px-4 py-2 rounded-2xl shadow"
+            className="bg-emerald-500 text-blue px-4 py-2 rounded-2xl shadow"
           >
             ▶︎ Word
           </button>
